02aca6eed06ccd094af23f3f774585f46232fcb2
Douglas Santos Pereira <douglas.pereira@dougnoMacBook-puro.local>
2019-10-15 15:15:52 -0300

    Estudo

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, AsyncStorage } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, Image, Text, AsyncStorage } from 'react-native';
+
+import SpotList from '../components/SpotList';
+
+import logo from '../assets/logo.png';
 
 export default function List() {
     const [techs, setTechs] = useState([]);
@@ -11,5 +15,26 @@ export default function List() {
             setTechs(techsArray);
         })
     }, []);
-    return <Text>{techs}</Text>
+
+    return (
+        <SafeAreaView style={StyleSheet.container}>
+            <Image style={styles.logo} source={logo} />
+            <ScrollView>
+                {techs.map(tech => <SpotList key={tech} tech={tech} />)}
+            </ScrollView>
+        </SafeAreaView>
+    )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+
+    logo: {
+        height: 32,
+        resizeMode: 'contain',
+        alignSelf: 'center',
+        marginTop: 10,
+    },
+});
