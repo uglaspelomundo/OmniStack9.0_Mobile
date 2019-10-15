40ec826b662a0c1c4a8ed32ebaea1a7a89869c43
Douglas Santos Pereira <douglas.pereira@dougnoMacBook-puro.local>
2019-10-14 21:11:58 -0300

    Estudo

---
+++ src/pages/List.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, AsyncStorage } from 'react-native';
+
+export default function List() {
+    const [techs, setTechs] = useState([]);
+
+    useEffect(() => {
+        AsyncStorage.getItem('techs').then(storagedTechs => {
+            const techsArray = storagedTechs.split(',').map(tech => tech.trim());
+
+            setTechs(techsArray);
+        })
+    }, []);
+    return <Text>{techs}</Text>
+}
